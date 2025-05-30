24c15c9f197b2c7f258b1043f2f9e6e8b23af731
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-05-30 15:25:28 +0800

    feat: refactor post page functionality; consolidate components and remove unused code

--- app/(app)/u/[username]/p/[post_id]/page.tsx
+++ app/(app)/u/[username]/p/[post_id]/page.tsx
@@ -1,28 +1,34 @@
-import PostPage from "@/components/post-page";
-import { getPost } from "@/data/get-post.server";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+"use client";
 
-export default async function Page({
+import CommentForm from "@/components/comment-form";
+import CommentList from "@/components/comment-list";
+import PostItem from "@/components/post-item";
+import { useUser } from "@/context/user-context";
+import { getPost } from "@/data/get-post";
+import { useQuery } from "@tanstack/react-query";
+import { use } from "react";
+
+export default function Page({
   params,
 }: {
   params: Promise<{ username: string; post_id: string }>;
 }) {
-  const { username, post_id } = await params;
+  const { username, post_id } = use(params);
 
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
+  const { data } = useQuery({
     queryKey: ["post", post_id],
-    queryFn: () => getPost({ post_id, username }),
+    queryFn: () => getPost({ username, post_id }),
   });
 
+  const { id } = useUser();
+
+  if (!data) return;
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <PostPage post_id={post_id} username={username} />
-    </HydrationBoundary>
+    <div className="flex h-full flex-col">
+      <PostItem post={data} current_user_id={id} />
+      <CommentList post_id={post_id} />
+      <CommentForm post_id={post_id} />
+    </div>
   );
 }
