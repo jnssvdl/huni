6716b6360b5b6cd0931e5c9bdbc7ea740d7fdf0c
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-06-09 22:33:41 +0800

    feat: implement infinite scrolling in GlobalFeed component using Intersection Observer

--- components/global-feed.tsx
+++ components/global-feed.tsx
@@ -8,6 +8,8 @@ import { useUser } from "@/context/user-context";
 import { FEED_LIMIT } from "@/constants";
 import PostSkeleton from "./post-skeleton";
 import { RotateCcw } from "lucide-react";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function GlobalFeed() {
   const user = useUser();
@@ -34,10 +36,17 @@ export default function GlobalFeed() {
     initialPageParam: 0,
   });
 
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
   const posts = data?.pages.flat() || [];
 
   if (isLoading) {
-    // Show skeletons while first page is loading
     return (
       <>
         {Array.from({ length: FEED_LIMIT }).map((_, i) => (
@@ -60,9 +69,18 @@ export default function GlobalFeed() {
 
   return (
     <>
-      {posts.map((post) => (
-        <PostItem key={post.post_id} post={post} user_id={user.id} />
-      ))}
+      {posts.map((post, index) =>
+        posts.length === index + 1 ? (
+          <PostItem
+            key={post.post_id}
+            post={post}
+            user_id={user.id}
+            innerRef={ref}
+          />
+        ) : (
+          <PostItem key={post.post_id} post={post} user_id={user.id} />
+        ),
+      )}
 
       {/* Show additional skeleton when fetching next page */}
       {isFetchingNextPage &&
@@ -71,19 +89,9 @@ export default function GlobalFeed() {
         ))}
 
       <div className="flex justify-center border-b p-4">
-        {hasNextPage ? (
-          <Button
-            variant={"ghost"}
-            onClick={() => fetchNextPage()}
-            disabled={isFetchingNextPage}
-          >
-            View more posts
-          </Button>
-        ) : (
-          <p className="text-muted-foreground text-sm">
-            You have reached the end of the page.
-          </p>
-        )}
+        <p className="text-muted-foreground text-sm">
+          You have reached the end of the feed.
+        </p>
       </div>
     </>
   );
